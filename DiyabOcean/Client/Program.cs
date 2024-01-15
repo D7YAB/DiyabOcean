e35e856d2a736b2279ada1c79bc2373aa79874a8
Diyab Hossain <hossaind@iccsolutions.com>
2024-01-15 10:44:09 +0000

    Created client ProductService Interface

--- DiyabOcean/Client/Program.cs
+++ DiyabOcean/Client/Program.cs
@@ -1,4 +1,7 @@
+global using DiyabOcean.Shared;
+global using System.Net.Http.Json;
 using DiyabOcean.Client;
+using DiyabOcean.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
