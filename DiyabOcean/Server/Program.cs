6661ab58d426350a7d066bb5b6f37f46290cba0e
Diyab Hossain <hossaind@iccsolutions.com>
2024-01-22 10:48:37 +0000

    Retrieve all Categories with CategoryService and CategoryController

--- DiyabOcean/Server/Program.cs
+++ DiyabOcean/Server/Program.cs
@@ -2,6 +2,7 @@ global using DiyabOcean.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using DiyabOcean.Server.Data;
 global using DiyabOcean.Server.Services.ProductService;
+global using DiyabOcean.Server.Services.CategoryService;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
