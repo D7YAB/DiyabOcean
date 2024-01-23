bba1923801af81c840eeb3b20492b81fc46956d5
Diyab Hossain <hossaind@iccsolutions.com>
2024-01-23 15:28:38 +0000

    Updated DataContext for Variants and ProductVariants

--- DiyabOcean/Server/Data/DataContext.cs
+++ DiyabOcean/Server/Data/DataContext.cs
@@ -9,6 +9,26 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ProductVariant>().HasKey(p => new { p.ProductId, p.VariantId });
+
+            modelBuilder.Entity<Variant>().HasData(
+                new Variant
+                {
+                    Id = 1,
+                    Name = "Default"
+                },
+                new Variant
+                {
+                    Id = 2,
+                    Name = "Chicken"
+                },
+                new Variant
+                {
+                    Id = 3,
+                    Name = "Lamb"
+                }
+                );
+
             modelBuilder.Entity<Category>().HasData(
                new Category
                {
@@ -25,7 +45,6 @@
                     Title = "Chaat",
                     Description = "Chaat, a traditional savoury snack sold by street vendors in India. A great starter or side dish served with home made puri.",
                     ImageUrl = "",
-                    Price = 4.25m,
                     CategoryId = 1
                 },
                 new Product
@@ -34,7 +53,6 @@
                     Title = "Chilli Chicken",
                     Description = "Tender & flavorful Chilli Chicken, made with chicken marinated in soy sauce, fried till crispy, sautéed with onions, peppers & sauces.",
                     ImageUrl = "",
-                    Price = 4.25m,
                     CategoryId = 1
                 },
                 new Product
@@ -43,13 +61,41 @@
                     Title = "Spicy Chicken Wings",
                     Description = "Spicy grilled chicken wings are everyone's favorite. Juicy chicken wings are marinated in hot sauce and grilled to perfection.",
                     ImageUrl = "",
-                    Price = 4.25m,
                     CategoryId = 1
+                }
+            );
+
+            modelBuilder.Entity<ProductVariant>().HasData(
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    VariantId = 2,
+                    Price = 3.45m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    VariantId = 3,
+                    Price = 3.95m
+                },
+                new ProductVariant
+                {
+                    ProductId = 2,
+                    VariantId = 1,
+                    Price = 3.95m
+                },
+                new ProductVariant
+                {
+                    ProductId = 3,
+                    VariantId = 1,
+                    Price = 3.95m
                 }
             );
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Variant> Variants { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
     }
 }
