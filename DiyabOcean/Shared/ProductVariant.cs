6e229d20eccffc1e0dff176c721ef35b3c217dcf
Diyab Hossain <hossaind@iccsolutions.com>
2024-01-23 14:55:35 +0000

    Added Variant and ProductVariant models

---
+++ DiyabOcean/Shared/ProductVariant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DiyabOcean.Shared
+{
+    public class ProductVariant
+    {
+        [JsonIgnore]
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+        public Variant Variant { get; set; }
+        public int VariantId { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal OriginalPrice { get; set; }
+
+    }
+}
